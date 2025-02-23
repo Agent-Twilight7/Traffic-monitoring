4377783c7ab69439c69f977969a5d2911dd13f67
Risa Niranjan Chaudhari <155613845+Agent-Twilight7@users.noreply.github.com>
2025-02-24 02:31:08 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+// export default App;
+import React from "react";
+import VideoStream from "./components/VideoStream";
+
+function App() {
+    return (
+        <div className="App">
+            <h1>AI Object Detection</h1>
+            <VideoStream />
+        </div>
+    );
+}
+
+export default App;
+
